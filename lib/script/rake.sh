268e5736e331c82bdac1b386dd375a01ef34ee33
jay16 <jay_li@intfocus.com>
2015-01-22 09:42:55 +0800

    view#bug fix

--- lib/script/rake.sh
+++ lib/script/rake.sh
@@ -3,4 +3,4 @@
 APP_ROOT_PATH=$1
 ENVIRONMENT=$2
 
-/bin/bash -l -c "cd ${APP_ROOT_PATH} && RACK_ENV=${ENVIRONMENT} bundle exec rake agent:main >> ./log/rake.log 2>&1"
+/bin/bash -l -c "cd ${APP_ROOT_PATH} && RACK_ENV=${ENVIRONMENT} bundle exec rake callback:main >> ./log/rake.log 2>&1"
