5efaac433a1f9ccc2ac21b4443afcd6f857ffae9
jay16 <jay_li@intfocus.com>
2015-11-16 20:09:18 +0800

    demo page optimized.

---
+++ app/assets/javascripts/demo_home.js
@@ -0,0 +1,42 @@
+(function() {
+  window.DemoHome = {
+    timerShowSkeleton: void 0,
+    showSkeleton: function(width) {
+      return [].forEach.call($("*"), function(a) {
+        return a.style.outline = (width > 0 ? width + "px solid #" + (~~(Math.random() * (1 << 24))).toString(16) : "none");
+      });
+    },
+    toggleSkeleton: function() {
+      if (typeof $.timerShowSkeleton === "undefined" || $.timerShowSkeleton === null) {
+        return $.timerShowSkeleton = self.setInterval("DemoHome.showSkeleton(1)", 500);
+      } else {
+        $.timerShowSkeleton = window.clearInterval($.timerShowSkeleton);
+        return DemoHome.showSkeleton(0);
+      }
+    }
+  };
+
+  $(function() {
+    var pathname, poetry;
+    pathname = window.location.pathname;
+    $(".post-href").each(function() {
+      var href;
+      href = $(this).attr("href");
+      if (!/^http:\/\/|^https:\/\//.test(href)) {
+        return $(this).attr("href", (pathname + href).replace("//", "/"));
+      }
+    });
+    poetry = "凡是遥远的地方,对我们来说都有一种诱惑,不是诱惑于美丽,就是诱惑于传说,即便远方的风景,并不如人意,我们也无需在乎,因为这实在是一个,迷人的错,到远方去 到远方去,熟悉的地方没有景色";
+    $('.site-name').css('height', $('.site-name').css('height'));
+    return $(".typed-quotes").typed({
+      strings: poetry.split(","),
+      typeSpeed: 100,
+      contentType: "text",
+      loop: true,
+      backDelay: 1000,
+      showCursor: true,
+      cursorChar: "|"
+    });
+  });
+
+}).call(this);
--- unicorn.sh
+++ unicorn.sh
@@ -12,10 +12,11 @@ shell_used=${SHELL##*/}
 if [[ "${shell_used}" == "sh" ]]; 
 then 
 	shell_used="bash"; 
+    [ -f ~/.${shell_used}_profile ] && source ~/.${shell_used}_profile &> /dev/null
 fi
 echo "## shell used: ${shell_used}"
-[ -f ~/.${shell_used}_profile ] && source ~/.${shell_used}_profile &> /dev/null
 [ -f ~/.${shell_used}rc ] && source ~/.${shell_used}rc &> /dev/null
+
 export LANG=zh_CN.UTF-8
 
 # put below config lines added to ~/.bashrc to make 
