e6a1e047f28f73e5ab8549099aff05c7907aa9b5
jay16 <jay_li@intfocus.com>
2016-04-10 18:26:07 +0800

    optimized: add rank process

--- app/controllers/demo/money_ball_controller.rb
+++ app/controllers/demo/money_ball_controller.rb
@@ -6,7 +6,7 @@ class Demo::MoneyBallController < Demo::ApplicationController
   helpers Demo::MoneyBallHelper
 
   before do
-    @page_header = '2016春季乐课力台球联赛'
+    @page_header = '16春乐课力台球联赛'
     @current_stage = '积分赛'
     @current_season = '16-mar'
     set_seo_meta(@page_header, "八球，2016春季，赛事", "八球，2016春季，赛事，乐课力")
@@ -48,16 +48,18 @@ class Demo::MoneyBallController < Demo::ApplicationController
   post '/' do
     players = [params[:player0], params[:player1]]
     scores = [params[:score0], params[:score1]].map(&:to_i)
-    if score0 < score1
+    if scores[0] < scores[1]
       players = players.reverse
       scores = scores.reverse
     end
     id = md5("#{Time.now.to_f}#{params}")
-    matches = parse_matches.push(id: id, players: players, scores: scores, stage: params[:stage], date: Time.now.strftime('%m/%d %H:%M'))
+    matches = json_parse(File.read(mb_json_path('matches')))
+    matches.push(id: id, players: players, scores: scores, stage: params[:stage], date: Time.now.strftime('%m/%d %H:%M'))
 
     File.open(mb_json_path('matches'), "w:utf-8") { |file| file.puts(matches.to_json) }
+    rank_matches
 
-    redirect to('edit')
+    redirect to('edits')
   end
 
   get '/:id/edit' do
@@ -79,6 +81,7 @@ class Demo::MoneyBallController < Demo::ApplicationController
     match[:stage] = params[:stage]
     matches[index] = match
     File.open(mb_json_path('matches'), "w:utf-8") { |file| file.puts(matches.to_json) }
+    rank_matches
 
     redirect to('edits')
   end
@@ -87,5 +90,50 @@ class Demo::MoneyBallController < Demo::ApplicationController
 
   def mb_json_path(filename)
     app_root_join("config/money_ball/#{@current_season}/#{filename}.json")
+  end
+
+  def rank_matches
+    results = Hash.new(0)
+    no_match_players = json_parse(File.read(mb_json_path('players'))).map { |p| p['name'] }
+    json_parse(File.read(mb_json_path('matches'))).each do |match|
+      if match['scores'][0] > match['scores'][1]
+        results[match['players'][0]] += 1
+        results[match['players'][1]] += 0
+      else
+        results[match['players'][0]] += 0
+        results[match['players'][1]] += 1
+      end
+
+      no_match_players -= match['players']
+    end
+
+    no_match_players.each do |player|
+      results[player] = -1
+    end
+
+    players = json_parse(File.read(mb_json_path('players')))
+    matches = json_parse(File.read(mb_json_path('matches')))
+    hash_result = results.map { |k, v|
+      player = players.select { |h| h['name'] == k }[0]
+      count = matches.select { |m| m['players'].include?(k) }.count
+      [k, v, count, player['company'], player['gender']] 
+    }.group_by { |a| a[1] }
+
+    rank_result = hash_result.map do |k, v|
+      [k.to_i, v]
+    end
+
+    result = []
+    rank_index = 1
+    rank_result.sort_by { |a| a[0] }.reverse_each do |a|
+      a[1].each do |play_result|
+        result.push(play_result.unshift(a[0] == -1 ? '-' : rank_index))
+      end
+
+      rank_index += 1 if a[0] > -1
+    end
+
+    json_path = mb_json_path('rank')
+    File.open(json_path, "w:utf-8") { |file| file.puts(result.to_json) }
   end
 end
